b70dc0570c19185aa24ef9c03ce90f90cb292069
Jackson Adams <jadams5985@sdsu.edu>
2025-09-20 17:36:52 -0700

    commented the given classes and functions in LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -8,39 +8,47 @@
 template<typename T>
 class Node {
 private:
-    T data;
-    Node<T>* next;
+    T data; // type T, can be anything
+    Node<T>* next; // A pointer node that will point to the next element
 
     // Grant LinkedList access to private members
     template<typename> friend class LinkedList;
 
 public:
-    Node(T val) : data(val), next(nullptr) {}
+    Node(T val) : data(val), next(nullptr) {} // constructor for the node that assigns the value to the data, and init the next node to a nullptr
 };
 
+
+// Linked List class template
 template<typename T>
 class LinkedList {
 private:
-    Node<T>* head;
+    Node<T>* head; // The linked list has a pointer that points to the first element
 
 public:
-    LinkedList() : head(nullptr) {}
+    LinkedList() : head(nullptr) {} // constructor for the list, init the head to a nullptr so it doesn't pick up some garbage val
 
+    // add an element to the back of the list
     void append(T value) {
 
     }
 
+    // add an element to the front of the list
     void addFirst(T value) {
 
     }
 
+    // remove the first element of the list
     void deleteFirst() {
 
     }
 
+    // remove the last element of the list
     void deleteLast() {
 
     }
+
+    // remove an element at a specific index
     void deleteAtIndex(int index) {
 
         // Else index was out of bounds, do nothing
